fdc1fda7bc5f388940a3f7208b202bd898d5d7e2
ukkarikari <guanchan-telecom@protonmail.com>
2026-06-10 20:08:19 -0300

    xmonad cleanup

--- xmonad/.config/xmonad/README.md
+++ xmonad/.config/xmonad/README.md
@@ -1,33 +1,10 @@
+<div align="center">
+
 # xmonad config
 
-###  CURRENT REQUIREMENTS!
- - picom-simple-anims-git (compositor)
- - xdotool (gambiarra)
- - redshift (lightness)
- - xclip + scrot (clipboard)
- - xss-lock + xtrlock (lockscreen)
- - dzen2
- - ProggyFonts, Unifont, cozette
+<br>
 
-### TO LEARN:
- - [ ] Xmonad specific types (like X() and logHook)
- - [ ] solidify your conceptual understanding of monads
+</div>
 
-### TO IMPLEMENT:
- - [ ] some way to toggle simpleFloat with keybind (instead of being a default)
- - [ ] keybind to change layout lists. different lists for differnt setups (like 4:3 16:9 vertical etc)
- - [x] reading + notes layout
- - [ ] make proper compositor config 
- - [ ] fix wallpaper code 
- - [x] add xresources?
- - [x] grid layout with custom keybind to toggle magnifier
- - [x] dzen bar basic
- - [ ] monitor https://hackage-content.haskell.org/package/xmonad-contrib-0.18.2/docs/XMonad-Layout-Monitor.html
- - [ ] theme for prompt
- - [ ] rdp layout
+personal strange xmonad configuration
 
- # --- configs ---
- - .Xresources
- - .xprofile
- - .redshift config
- - .picom-config
--- xmonad/.config/xmonad/xmonad.hs
+++ xmonad/.config/xmonad/xmonad.hs
@@ -1,39 +1,31 @@
 
 import Data.Ratio
 import System.IO (hPutStrLn)
+
 import XMonad
-import XMonad.Actions.GridSelect
-import XMonad.Actions.UpdatePointer
-import XMonad.Actions.WindowBringer
-import XMonad.Actions.FloatSnap
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.InsertPosition
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.ManageHelpers
-import XMonad.Hooks.StatusBar
-import XMonad.Hooks.RefocusLast
-import XMonad.Hooks.Place
+
 import XMonad.Layout.Accordion
 import XMonad.Layout.CircleEx
 import XMonad.Layout.Gaps
 import XMonad.Layout.IfMax
 import XMonad.Layout.Magnifier
-import XMonad.Layout.MultiDishes
-import XMonad.Layout.NoBorders
 import XMonad.Layout.PerWorkspace 
--- import XMonad.Layout.Renamed
 import qualified XMonad.Layout.Renamed as Renamed
-import XMonad.Layout.ResizableThreeColumns
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.StackTile
 import XMonad.Layout.Tabbed
 import XMonad.Layout.SimpleFloat
-import XMonad.Layout.NoFrillsDecoration
 import XMonad.Layout.SimplestFloat
+
+import XMonad.Layout.NoFrillsDecoration
 import XMonad.Layout.DecorationMadness
 import XMonad.Layout.Decoration
 import XMonad.Layout.SimpleDecoration
+
+import XMonad.Actions.GridSelect
+import XMonad.Actions.UpdatePointer
+
+import XMonad.Layout.Column
 import XMonad.Layout.HintedGrid
 import XMonad.Layout.IM
 import XMonad.Layout.WindowArranger
@@ -42,24 +34,27 @@ import XMonad.Layout.WindowNavigation
 import XMonad.Layout.Simplest
 import XMonad.Layout.FocusTracking
 import XMonad.Layout.LimitWindows
-import XMonad.Layout.BoringWindows
 import XMonad.Layout.Roledex
 import XMonad.Layout.TwoPanePersistent
 import XMonad.Layout.AutoMaster
 import XMonad.Layout.Reflect
 import XMonad.Layout.CenteredMaster
-import XMonad.Layout.Column
+import XMonad.Layout.Spiral
+
 import XMonad.Actions.PerLayoutKeys
 import XMonad.Actions.PerWorkspaceKeys
+
+import XMonad.Layout.BoringWindows
 import XMonad.Layout.PerScreen
-import XMonad.Layout.Spiral
+import XMonad.Layout.NoBorders
+import qualified XMonad.Layout.MultiToggle as MT
+import XMonad.Layout.MultiToggle.Instances
 import XMonad.Layout.Spacing
 import qualified XMonad.Layout.BoringWindows as BW
 import XMonad.Actions.MouseResize
 import qualified XMonad.StackSet as W
 import XMonad.Util.EZConfig
 
-import XMonad.Util.NamedScratchpad
 import XMonad.Util.Run (runProcessWithInput, safeSpawn, spawnPipe)
 import XMonad.Util.SpawnOnce
 import XMonad.Prompt
@@ -67,67 +62,70 @@ import XMonad.Prompt.Input
 import XMonad.Prompt.Shell
 import XMonad.Prompt.Pass
 import XMonad.Prompt.Workspace
+
 import Config.GridSelect
 
--- ++++++++++ MAIN +++++++++++
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.InsertPosition
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.RefocusLast
+import XMonad.Hooks.Place
+
+
+--     ---- index ----
+-- 
+-- main
+-- workspaces
+-- layouts
+-- appearance
+-- utilites
+-- hooks
+-- keybindings
+-- 
+--     ----------------
+
+
+-- =========================================================================
+--                                 MAIN                                   
+-- =========================================================================
 main :: IO ()
 main = do
-  spawn "xrdb -merge $HOME/.Xresources"
   dzen <- spawnPipe myDzenCmd
   xmonad
     . docks
     . ewmh
     $ myConfig dzen
 
--- ========= PRETTY PRINTER and DZEN  =========
--- [main] -> [myConfig] -> [logHook] -> [myPP] -> [dzen2 process]
--- ============================================
 
--- ---------- dzen command ----------
-myDzenCmd :: String
--- myDzenCmd =
---   "dzen2"
---     ++ " -dock"
---     ++ " -ta r"
---     ++ " -fn Cozette:bold:size=10"
---     ++ " -bg #000000"
---     ++ " -fg #ffffff"
-
-myDzenCmd = "" --temp 
-
--- ---------- pretty printer ----------
-myPP h =
+myConfig dzen =
   def
-    { ppOutput = hPutStrLn h,
-      ppOrder = \(ws : l : t : ex) -> [l, t, ws] ++ ex,
-      ppCurrent = dzenColor "#000000" "#f9f9f9" . wrap " " " ",
-      ppHidden = wrap " " " ",
-      ppSep = " ",
-      ppExtras = [myCommand, myVolume, mySpace]
+    { modMask = mod4Mask, -- rebind alt to win
+      layoutHook = myLayouts,
+      manageHook = myManageHook <+> manageDocks <+> placeHook simpleSmart <+> manageHook def,
+      startupHook = myStartupHook,
+      logHook = do
+        refocusLastLogHook
+        updatePointer (0.5, 0.5) (0, 0)
+        dynamicLogWithPP (myPP dzen),
+      handleEventHook =
+        refocusLastWhen (pure True)
+        <> handleEventHook def,
+      workspaces = myWorkspaces,
+      focusFollowsMouse = False,
+      terminal = "urxvt",
+      normalBorderColor = "#888888",
+      focusedBorderColor = "#ffffff"
     }
+    `removeKeysP` myRemovedKeys
+    `additionalKeysP` myKeybs
 
--- just another space
-mySpace :: X (Maybe String)
-mySpace = do
-  return (Just (" "))
 
--- command segment
-myCommand :: X (Maybe String)
-myCommand = do
-  result <- runProcessWithInput "date" [] ""
-  return (Just (init result))
-
--- volume indicator
-myVolume :: X (Maybe String)
-myVolume = do
-  out <- runProcessWithInput "wpctl" ["get-volume", "@DEFAULT_AUDIO_SINK@"] ""
-  let status =
-        if "[MUTED]" `elem` words out
-          then "☏"
-          else "☎"
-  return (Just status)
-
--- ========== WORKSPACES =======
+-- =========================================================================
+--                             WORKSPACE-LAYOUTS                         
+-- =========================================================================
 myWorkspaces :: [WorkspaceId]
 myWorkspaces =
   [ "code",
@@ -135,126 +133,73 @@ myWorkspaces =
     "code_alt",
     "rdp",
     "aux",
-    "write"
+    "research",
+    "media"
   ]
 
---  ========= LAYOUTS =========
-myLayouts = 
-  onWorkspace "code" codeLayouts $
-  onWorkspace "code_alt" codeAltLayouts $
-  onWorkspace "web" webLayouts $
-  onWorkspace "aux" auxLayouts $
-  onWorkspace "rdp" rdpLayouts $
-  onWorkspace "write" writeLayouts $ 
-  defaultLayout
+myLayouts =
+  MT.mkToggle (MT.single MIRROR) $
+    onWorkspace "code" codeLayouts $
+    onWorkspace "code_alt" codeAltLayouts $
+    onWorkspace "web" webLayouts $
+    onWorkspace "aux" auxLayouts $
+    onWorkspace "research" researchLayouts $ 
+    onWorkspace "media" mediaLayouts 
+    defaultLayout
 
-codeLayouts =
-  boringAuto ( limitWindows 2  ( noBorders (magnifiercz' 1.3 (ResizableTall 1 (3 / 100) (9 / 16) [])) ) )
-  ||| boringWindows columnSelector
-  -- ||| boringWindows (noBorders Simplest)
-
-codeAltLayouts =
-  boringAuto ( limitWindows 2  ( noBorders (magnifiercz' 1.3 (ResizableTall 1 (3 / 100) (9 / 16) [])) ) )
-  ||| boringWindows circleSelector
-  -- ||| boringWindows ( noBorders Simplest )
-
-webLayouts =
-  boringAuto ( limitWindows 2  ( noBorders (ResizableTall 1 (3 / 100) (9 / 16) [])) )
-  ||| boringWindows roledexSelector
-  ||| boringWindows ( noBorders Simplest )
-
-auxLayouts =
-  boringWindows ( spiral (9/10) )
-  ||| boringWindows (spacingWithEdge 10 (spiral (9/10)))
-  ||| myFloat
-       
-rdpLayouts =
-      myFloat
-  ||| noBorders (tabbedBottom shrinkText myTabTheme)
-
-writeLayouts =
-  boringWindows (magnifiercz' 1.3 (TwoPanePersistent Nothing (3/100) (9/16)) ) 
+codeLayouts = boringWindows $
+      twoPaneThing 2 (9/16)
   ||| noBorders Simplest
 
-defaultLayout =
+codeAltLayouts = boringWindows $
+      twoPaneThing 2 (9/16)
+  ||| noBorders Simplest
+
+webLayouts = boringWindows $
+       twoPaneThing 2 (1/2)
+   ||| noBorders (tabbedBottom shrinkText myTabTheme)
+
+auxLayouts = boringWindows $
+      spiral (9/10) 
+  ||| myDecorate simplestFloat
+       
+researchLayouts = boringWindows $
+      twoPaneThing 3 (1/2)
+  ||| noBorders Simplest
+
+mediaLayouts = boringWindows $
+  roledexGapDeco
+  ||| boringWindows (noBorders Simplest)
+  
+defaultLayout = boringWindows $
   noBorders Simplest
-  ||| simpleFloat' shrinkText myTabTheme
+  ||| myDecorate simplestFloat
 
 
---  --------- definitions ---------
+-- =========================================================================
+--                                LAYOUTS  
+-- =========================================================================
 
-myFloat =
-  Renamed.renamed [Renamed.CutWordsLeft 10, Renamed.Replace "My Float"] $
-    boringAuto ( mouseResize $ noFrillsDeco shrinkText myTabTheme (magnifierczOff' 1.3 simplestFloat) )
+{-# ANN myDecorate ("HLint: ignore Eta reduce" :: String) #-}
 
--- ------ weird old circle layout
-meinKreis =
-  ifWider 1280 (
-  gaps 
-      [(L, 160), (R, 200), (U, 30), (D, 5)]
-        circleEx
-          { cMasterRatio = 8 % 8,
-            cStackRatio = 4 % 8,
-            cMultiplier = 5 % 7,
-            cDelta = -2.2 * pi / 4,
-            cNMaster = 0
-          }
-  ) $
-  gaps 
-      [(L, 10), (R, 10), (U, 35), (D, 5)]
-        circleEx
-          { cMasterRatio = 6 % 8,
-            cStackRatio = 4 % 8,
-            cMultiplier = 5 % 7,
-            cDelta = -2.2 * pi / 4,
-            cNMaster = 0
-          }
-  
+-- helpers
+myDecorate l = -- the l in this decorate function is to fix the 'a0' ambiguity error
+  noFrillsDeco shrinkText myTabTheme l
 
--- ------ circleFloat with my decorator
-circleFloatResizable =
-  circleDefaultResizable shrinkText myTabTheme
-
--- ------ weird sort of window order selector to pair with two pane
-columnSelector = 
-  noFrillsDeco shrinkText myTabTheme
-    ( spacingWithEdge 2   
-      ( gaps [(L, 20), (R, 20), (U, 20), (D, 20)] ( magnifierxyOff' 1.6 1.8
-        -- ( Mirror (autoMaster 1 (5/100)  
-               -- (Mirror $ Column 1.6) )))))
-               (spiral (11/12) ) )))
+-- my layout definitions
+twoPaneThing win_n ratio = 
+  limitWindows win_n ( noBorders (magnifierczOff' 1.3 (ResizableTall 1 (3 / 100) ratio [])) ) 
+ 
+roledexGapDeco =
+  myDecorate 
+   ( gaps [(L, 30), (R, 30), (U, 10), (D, 10)]
+       (reflectVert Roledex)
+   )
 
 
--- ------ same window order selector thing but for cirlce
-circleSelector = 
-  noFrillsDeco shrinkText myTabTheme
-    ( magnifierxyOff' 1.5 1.7 
-      ( spacingWithEdge 5 ( gaps [(L, 20), (R, 20), (U, 20), (D, 20)]
-        ( Mirror ( autoMaster 1 (5/100)
-          meinKreis )))))
-
--- ------- master window + roledex experiment
-roledexSelector =
-       noFrillsDeco shrinkText myTabTheme
-         ( spacingWithEdge 5 ( gaps [(L, 20), (R, 20), (U, 20), (D, 20)]
-           ( Mirror (autoMaster 1 (1/100)  $
-                  magnifierxyOff 1.4 1.5 (reflectVert Roledex)  ))))
-
-
---  ------ weird ifMax abomination
--- ( IfMax 2 (noBorders (magnifiercz' 1.3 (ResizableTall 1 (3 / 100) (3 / 5) []))) $
---     IfMax 3 (maximizeVertical (MultiDishes 2 3 (1 / 8))) $
---       maxMagnifierOff ( Grid False )
--- )
-
-
---  ----- when i made TwoPane with BoringWindows because i didnt know TwoPane existed
--- boringAuto ( limitWindows 2  ( noBorders (magnifiercz' 1.3 (ResizableTall 1 (3 / 100) (9 / 16) [])) ) )
--- ------ this one is the same but looks cooler after you switch to floating
--- boringAuto ( limitWindows 3  ( noBorders (magnifiercz' 1.3 (ResizableTall 1 (3 / 100) (9 / 16) [])) ) )
--- ------ hmmm i think it may actually work better than TwoPane. ill save it just in case though
--- boringWindows ( magnifierczOff' 1.3 ( TwoPanePersistent Nothing (3/100) (9/16) ) ) 
-  
+-- =========================================================================
+--                                APPEARANCE  
+-- =========================================================================
 
 -- -------- decorator themes -----------
 myTabTheme :: Theme
@@ -283,7 +228,44 @@ myXPConfig = def
   , historySize         = 100
   }
 
--- ========= interesting renaming window utility =======
+-- ----------------- PRETTY PRINTER and DZEN --------------------
+-- [main] -> [myConfig] -> [logHook] -> [myPP] -> [dzen2 process]
+-- --------------------------------------------------------------
+
+-- ---------- dzen command ----------
+myDzenCmd :: String
+-- myDzenCmd =
+--   "dzen2"
+--     ++ " -dock"
+--     ++ " -ta r"
+--     ++ " -fn Cozette:bold:size=10"
+--     ++ " -bg #000000"
+--     ++ " -fg #ffffff"
+
+myDzenCmd = "" --temp 
+
+-- ---------- pretty printer ----------
+myPP h =
+  def
+    { ppOutput = hPutStrLn h,
+      ppOrder = \(ws : l : t : ex) -> [l, t, ws] ++ ex,
+      ppCurrent = dzenColor "#000000" "#f9f9f9" . wrap " " " ",
+      ppHidden = wrap " " " ",
+      ppSep = " ",
+      ppExtras = [mySpace]
+    }
+
+-- just another space
+mySpace :: X (Maybe String)
+mySpace = do
+  return (Just (" "))
+
+
+-- =========================================================================
+--                                UTILITIES
+-- =========================================================================
+
+-- --------- interesting renaming window utility (useful for urxvt) ---------
 renameFocusedPrompt :: XPConfig -> X ()
 renameFocusedPrompt conf =
     inputPrompt conf "Rename window" ?+ \title ->
@@ -291,7 +273,12 @@ renameFocusedPrompt conf =
             safeSpawn "wmctrl"
                 ["-i", "-r", show w, "-T", title]
 
--- ========= STARTUP HOOK =========
+
+-- =========================================================================
+--                                    HOOKS
+-- =========================================================================
+
+-- ------- startup hook -------
 myStartupHook = do
   spawnOnce "redshift -r -l manual"
   mapM_
@@ -304,7 +291,7 @@ myStartupHook = do
       -- , "sleep 2 ; xdotool search --name \"wpp\" windowlower windowsize 1440 900 windowmove 0 0"
     ]
 
---  ========= MANAGE HOOK =========
+--  -------  manage hook ------
 myManageHook :: ManageHook
 myManageHook =
   composeAll
@@ -316,19 +303,24 @@ myManageHook =
     ]
     <+> insertPosition Below Newer
 
---  ========= KEYBINDS =========
+
+-- =====================================================================
+--                                KEYBINDS                            
+-- =====================================================================
+
 myKeybs =
   windowKeybs
     ++ utilityKeybs
     ++ miscKeybs
 
--- ------------------------------
 windowKeybs =
   [
     -- overwrite with boring windows
       ("M-k", BW.focusUp)
     , ("M-j", BW.focusDown)
     , ("M-m", BW.focusMaster)
+    , ("M-b", BW.markBoring)
+    , ("M-S-b", BW.clearBoring)
     -- increase/decrease slave size
     , ("M-z", sendMessage MirrorShrink)
     , ("M-a", sendMessage MirrorExpand)
@@ -337,7 +329,7 @@ windowKeybs =
     , ("M-S-=", sendMessage MagnifyMore)
     , ("M--", sendMessage MagnifyLess)
     -- toggle doc
-    , ("M-S-m", sendMessage ToggleStruts)
+    -- , ("M-S-m", sendMessage ToggleStruts)
     -- ("M-g", withFocused $ snapShrink D Nothing >> snapShrink R Nothing
     --
     -- move floating windo
@@ -354,6 +346,8 @@ windowKeybs =
     -- move windows (previously overwritten by the move floating window keybs)
     -- , ("M-S-<Up>", windows W.swapUp)
     -- , ("M-S-<Down>", windows W.swapDown)
+    -- mirror
+    , ("M-S-m", sendMessage $ MT.Toggle MIRROR)
   ]
 
 utilityKeybs =
@@ -371,7 +365,7 @@ utilityKeybs =
     , ("M-S-p", passPrompt myXPConfig)
     , ("M-p", shellPrompt myXPConfig)
       -- rename winodw
-    , ("M-S-r", renameFocusedPrompt myXPConfig)
+    , ("C-S-r", renameFocusedPrompt myXPConfig)
   ]
 
 miscKeybs =
@@ -380,7 +374,7 @@ miscKeybs =
     ("M-<Tab>", myWorkspaceSelector myGSConfig)
   , ("M-S-<Tab>", bringSelected def)
   -- toggle mic feedback
-  , ("M-S-m", spawn "sh -c 'ID=$(pactl list short modules | grep module-loopback | cut -f1 | head -n1); [ -n \"$ID\" ] && pactl unload-module \"$ID\" || pactl load-module module-loopback latency_msec=1'")
+  , ("M-C-m", spawn "sh -c 'ID=$(pactl list short modules | grep module-loopback | cut -f1 | head -n1); [ -n \"$ID\" ] && pactl unload-module \"$ID\" || pactl load-module module-loopback latency_msec=1'")
   -- spawn stuff on aux
   , ("<F2>", bindOn [("aux", do
                           spawn "urxvt -e sh -c 'btop; bash'"
@@ -408,27 +402,3 @@ myRemovedKeys =
 
 moveAndFollow ws =
   windows (W.shift ws)
-
-
--- ++++++++++ CONFIGURATION +++++++++
-myConfig dzen =
-  def
-    { modMask = mod4Mask, -- rebind alt to win
-      layoutHook = myLayouts,
-      manageHook = myManageHook <+> manageDocks <+> placeHook simpleSmart <+> manageHook def,
-      startupHook = myStartupHook,
-      logHook = do
-        refocusLastLogHook
-        updatePointer (0.5, 0.5) (0, 0)
-        dynamicLogWithPP (myPP dzen),
-      handleEventHook =
-        refocusLastWhen (pure True)
-        <> handleEventHook def,
-      workspaces = myWorkspaces,
-      focusFollowsMouse = False,
-      terminal = "urxvt",
-      normalBorderColor = "#888888",
-      focusedBorderColor = "#ffffff"
-    }
-    `removeKeysP` myRemovedKeys
-    `additionalKeysP` myKeybs
